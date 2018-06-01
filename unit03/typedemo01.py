3e968aafb8c65bac8f14f5fd836ffd54ce824562
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 08:57:55 +0800

    Create typedemo01.py

---
+++ unit03/typedemo01.py
@@ -0,0 +1,10 @@
+# 印出 0.2 比例的兩個整數
+print(0.2.as_integer_ratio())
+# 判斷 0.235 是否為整數
+print(0.235.is_integer())
+# 判斷 2.000 是否為整數
+print(2.000.is_integer())
+
+# 檔名: typedemo01.py
+# 作者: Kaiching Chang
+# 時間: July, 2014
