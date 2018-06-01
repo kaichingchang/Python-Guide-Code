fd72c82e3400afc9c59e5b10699638cbdbb52e36
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 09:03:50 +0800

    Create exp_demo04.py

---
+++ unit05/exp_demo04.py
@@ -0,0 +1,11 @@
+a = 2
+b = a + 2  # b 會等於 4
+c = b - 2  # c 會等於 2
+d = c * 2  # d 會等於 4
+e = d / 2  # e 會等於 2.0
+f = e % 2  # f 會等於 0.0
+g = f ** 2 # g 會等於 0.0
+
+# 檔名: exp_demo04.py 
+# 作者: Kaiching Chang
+# 時間: July, 2014
