8417a1afba41e843c9bf1c5e3fa65763286f6c55
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 09:33:52 +0800

    Add files via upload

---
+++ unit22/tk_demo2.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.title("定位版面管理員")
+root.geometry("276x86")
+text = Label(root, text="Hello World!",
+             width="30", height="5",
+             bg="black", fg="white")
+text.place(x=0, y=0)
+root.mainloop()
+
+# 檔名: tk_demo2.py
+# 作者: Kaiching Chang
+# 時間: May, 2018
---
+++ unit22/tk_demo3.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title("格子版面管理員")
+text = Label(root, text="Hello World!",
+             width="30", height="5",
+             bg="black", fg="white")
+text.grid(row=0, column=0)
+root.mainloop()
+
+# 檔名: tk_demo3.py
+# 作者: Kaiching Chang
+# 時間: May, 2018
