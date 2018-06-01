da0136a119d2c1ed0d1fdd35d1a2502130998f41
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 08:58:51 +0800

    Create typedemo03.py

---
+++ unit03/typedemo03.py
@@ -0,0 +1,16 @@
+# 將 a 設定為 None
+a = None
+# 將 b 設定為 True
+b = True
+# 將 c 設定為 False
+c = False
+# 印出 a
+print(a)
+# 印出 b
+print(b)
+# 印出 c
+print(c)
+
+# 檔名: typedemo03.py
+# 作者: Kaiching Chang
+# 時間: July, 2014
