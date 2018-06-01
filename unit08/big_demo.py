771a7efd70fa4954efaf1d50de7a3c4c96e540c9
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 09:17:23 +0800

    Create big_demo.py

---
+++ unit08/big_demo.py
@@ -0,0 +1,14 @@
+def big(a, b):
+  if a > b:
+    return a
+  else:
+    return b
+
+print()
+print(big(33, 22))
+print(big("John", "Mary"))
+print()
+
+# 檔名: big_demo.py 
+# 作者: Kaiching Chang
+# 時間: July, 2014
