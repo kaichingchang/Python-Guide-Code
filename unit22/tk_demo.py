64ac0832a5c26c4976c9b0a9e9355189d1fb8635
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 09:33:39 +0800

    Create tk_demo.py

---
+++ unit22/tk_demo.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title("包裹版面管理員")
+text = Label(root, text="Hello World!",
+             width="30", height="5",
+             bg="black", fg="white")
+text.pack()
+root.mainloop()
+
+# 檔名: tk_demo.py
+# 作者: Kaiching Chang
+# 時間: July, 2014
