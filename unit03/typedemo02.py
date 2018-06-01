1756b6a17057da1bd7f1e4dd46bc5b51fb88489b
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 08:58:21 +0800

    Create typedemo02.py

---
+++ unit03/typedemo02.py
@@ -0,0 +1,10 @@
+# 印出 Hello world!
+print("Hello world!")
+# 印出 Hello    world!
+print("Hello\tworld!")
+# 印出 John's Treasure!
+print("John's Treasure!")
+
+# 檔名: typedemo02.py
+# 作者: Kaiching Chang
+# 時間: July, 2014
