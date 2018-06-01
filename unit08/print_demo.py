6ecebb99a40546c86c26a9cc2709bb9bc259f5ef
Kaiching Chang <kaichingc@gmail.com>
2018-06-01 09:17:34 +0800

    Add files via upload

---
+++ unit08/print_demo.py
@@ -0,0 +1,17 @@
+def print_newline():
+  print()
+
+def print_something(a):
+  print(a)
+
+def return_something(a):
+  return a
+
+print_newline()
+s = "There is no spoon."
+print_something(return_something(s))
+print_newline()
+
+# 檔名: print_demo.py 
+# 作者: Kaiching Chang
+# 時間: July, 2014
