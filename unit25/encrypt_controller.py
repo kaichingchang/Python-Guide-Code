5e408574c073ed9c954ff807c9ffcbd1b5b1818d
Kaiching Chang <kaichingc@gmail.com>
2018-06-05 13:40:39 +0800

    Update encrypt_controller.py

--- unit25/encrypt_controller.py
+++ unit25/encrypt_controller.py
@@ -1,7 +1,6 @@
 from tkinter import Tk
 from encrypt import Encrypt
 from encrypt_view import EncryptView
-import os
 
 # Encrypt 的 Controller 類別
 class EncryptController:
@@ -28,31 +27,11 @@ class EncryptController:
 
   # 按下載入按鈕的事件
   def lm(self):
-    # 先測試檔案是否存在
-    if os.path.exists("./code.txt"):
-      # 檔案存在就進行載入工作
-      f = open('./code.txt', 'r')
-      code = f.readline()
-      self.e = Encrypt(code)
-      s = str("".join(self.e.code))
-      m = "密碼表： " + s
-      self.app.dt["text"] = m
-    else:
-      m = "無法載入！"
-      self.app.dt["text"] = m
+    self.app.dt["text"] = "觸發載入按鈕的事件。"
 
   # 按下儲存按鈕的事件
   def sm(self):
-    # 先測試是否有按過新建按鈕
-    if self.e == None:
-      m = "無法儲存！"
-      self.app.dt["text"] = m
-    else:
-      # 有按過新建按鈕就進行存檔工作
-      f = open('./code.txt', 'w')
-      f.write("".join(self.e.code))
-      f.closed
-      self.app.dt["text"] = "完成存檔！"
+    self.app.dt["text"] = "觸發儲存按鈕的事件。"
 
   # 按下編碼按鈕的事件
   def em(self):
@@ -104,27 +83,13 @@ class EncryptController:
         m = "解碼成功！"
         self.app.dt["text"] = m
 
-  # 按下清楚按鈕的事件
+  # 按下清除按鈕的事件
   def cm(self):
-    self.e = None
-    self.userinput = ""
-    self.result = ""
-    self.app.ifd.delete(0, 200)
-    self.app.ofd.delete(0, 200)
-
-    self.app.dt["text"] = "已清除！"
+    self.app.dt["text"] = "觸發清除按鈕的事件。"
 
   # 按下拷貝按鈕的事件
   def cm2(self):
-    if self.result == "":
-      m = "無法拷貝！"
-      self.app.dt["text"] = m
-    else:
-      self.app.clipboard_clear()
-      r = self.result
-      self.app.clipboard_append(r)
-      m = "已拷貝！"
-      self.app.dt["text"] = m
+    self.app.dt["text"] = "觸發拷貝按鈕的事件。"
 
 # 執行部分
 if __name__ == '__main__':
