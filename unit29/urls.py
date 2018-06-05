0a82439fe5fc79c7c70525ec24a1f9c937cf60a9
Kaiching Chang <kaichingc@gmail.com>
2018-06-05 13:51:32 +0800

    Create urls.py

---
+++ unit29/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# 檔名: urls.py
+# 作者: Django startproject
+# 時間: June, 2018
