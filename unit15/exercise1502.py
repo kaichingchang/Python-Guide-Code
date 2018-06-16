8bf44b187be07d9cae2fad4c98cf700e1123308b
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 14:34:15 +0800

    Create exercise1502.py

---
+++ unit15/exercise1502.py
@@ -0,0 +1,12 @@
+nswer = "1234"
+ 
+if __name__ == "__main__":
+   guess = input(": ")
+   if guess == answer:
+      print("Right!!")
+   else:
+      print("Wrong!!")
+  
+# 檔名: exercise1502.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
