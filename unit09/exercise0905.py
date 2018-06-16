8b6d868b957a6f96954c08bae25f8bc6a734acb0
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 11:15:18 +0800

    Create exercise0905.py

---
+++ unit09/exercise0905.py
@@ -0,0 +1,27 @@
+class IntegerDemo:
+   def set_value(self, v):
+      self.value = v
+ 
+   def add(self, p):
+      self.value += p
+    
+   def subtract(self, p):
+      self.value -= p
+ 
+   def multiply(self, p):
+      self.value *= p
+ 
+   def divide(self, p):
+      self.value /= p
+ 
+i = IntegerDemo()
+i.set_value(int(input("v: ")))
+i.add(int(input("+: ")))
+i.subtract(int(input("-: ")))
+i.multiply(int(input("*: ")))
+i.divide(int(input("/: ")))
+print(i.value)
+  
+# 檔名: exercise0905.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
