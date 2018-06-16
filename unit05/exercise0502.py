faa01fdf54ad27849592d86dd40cd1d026515a9e
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 10:31:55 +0800

    Create exercise0502.py

---
+++ unit05/exercise0502.py
@@ -0,0 +1,11 @@
+a = int(input("Please input the first number: "))
+b = int(input("Please input the second number: "))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+  
+# 檔名: exercise0502.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
