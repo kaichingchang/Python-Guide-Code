5f4d6eb29f3e78d8c8eef1bfd11379b036238971
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 10:42:55 +0800

    Create exercise0603.py

---
+++ unit06/exercise0603.py
@@ -0,0 +1,19 @@
+g = int(input("g: "))
+if g < 60:
+   print("Bad!")
+elif 60 <= g < 70:
+   print("Not Bad")
+elif 70 <= g < 80:
+   print("Good!")
+elif 80 <= g < 90:
+   print("Great!")
+elif 90 <= g < 100:
+   print("Excellent!")
+elif g == 100:
+   print("Perfect!")
+else:
+   print("Pardon?")
+ 
+# 檔名: exercise0603.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
