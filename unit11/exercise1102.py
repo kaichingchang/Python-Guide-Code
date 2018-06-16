33a71fff71253e9643f869b2b0f295e0f9a939d4
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 11:31:54 +0800

    Create exercise1102.py

---
+++ unit11/exercise1102.py
@@ -0,0 +1,22 @@
+class FactorialDemo:
+   def __init__(self, n):
+      self.set_value(n)
+ 
+   def set_value(self, n):
+      i = 1
+      p = 1
+      while i <= n:
+         p *= i
+         i += 1
+ 
+      self.__value = p
+ 
+   def get_value(self):
+      return self.__value
+ 
+i = FactorialDemo(int(input("n: ")))
+print(i.get_value())
+  
+# 檔名: exercise1102.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
