d66e88b55f157f5c4b933e15478d5cad344743d4
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 11:06:49 +0800

    Create exercise0803.py

---
+++ unit08/exercise0803.py
@@ -0,0 +1,16 @@
+def sum(p):
+   i = 1
+   s = 0
+   while i <= p:
+      s += i
+      i += 1
+    
+   return s
+ 
+print(sum(10))
+print(sum(11))
+print(sum(12))
+  
+# 檔名: exercise0803.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
