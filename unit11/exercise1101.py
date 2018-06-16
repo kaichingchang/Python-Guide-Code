23c100063f5096649014ead7e15d545377fea2b3
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 11:31:33 +0800

    Create exercise1101.py

---
+++ unit11/exercise1101.py
@@ -0,0 +1,32 @@
+class IntegerDemo:
+   def __init__(self, v):
+      self.__value = v
+ 
+   def set_value(self, v):
+      self.__value = v
+ 
+   def get_value(self):
+      return self.__value
+ 
+   def add(self, p):
+      self.__value += p
+    
+   def subtract(self, p):
+      self.__value -= p
+ 
+   def multiply(self, p):
+      self.__value *= p
+ 
+   def divide(self, p):
+      self.__value /= p
+ 
+i = IntegerDemo(int(input("v: ")))
+i.add(int(input("+: ")))
+i.subtract(int(input("-: ")))
+i.multiply(int(input("*: ")))
+i.divide(int(input("/: ")))
+print(i.get_value())
+  
+# 檔名: exercise1101.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
