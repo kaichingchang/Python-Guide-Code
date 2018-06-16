6aa8b423df6bd6530a954d3dabbc6b8492f687fc
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 11:07:34 +0800

    Create exercise0805.py

---
+++ unit08/exercise0805.py
@@ -0,0 +1,16 @@
+def factorial(n):
+   i = 1
+   p = 1
+   while i <= n:
+      p *= i
+      i += 1 
+    
+   return p
+ 
+print(factorial(10))
+print(factorial(11))
+print(factorial(12))
+  
+# 檔名: exercise0805.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
