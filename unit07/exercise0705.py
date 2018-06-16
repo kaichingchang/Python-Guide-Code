2195270acf640bd1412afb5962b555aa9ee7cf85
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 10:47:16 +0800

    Create exercise0705.py

---
+++ unit07/exercise0705.py
@@ -0,0 +1,15 @@
+i = 0
+j = int(input("len: "))
+k = j
+while i < j:
+   while j > i:
+      print("*", end="")
+      j -= 1
+ 
+   print()
+   j = k
+   i += 1
+  
+# 檔名: exercise0705.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
