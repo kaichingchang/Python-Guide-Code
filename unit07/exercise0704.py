362b549a8182c28ee26645a7eceeb16934d1b85f
Kaiching Chang <kaichingc@gmail.com>
2018-06-16 10:46:56 +0800

    Create exercise0704.py

---
+++ unit07/exercise0704.py
@@ -0,0 +1,14 @@
+i = 0
+j = 5
+while i < 5:
+   while j > i:
+      print("*", end="")
+      j -= 1
+ 
+   print()
+   j = 5
+   i += 1
+  
+# 檔名: exercise0704.py 
+# 作者: Kaiching Chang 
+# 時間: July, 2014
